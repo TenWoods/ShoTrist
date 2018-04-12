147743a30f7da5784c68b19590240a991064875d
hust_Mo <m15048619182@163.com>
2018-04-12 10:44:17 +0800

    通过Import package的方式进行冲突修改

---
+++ Assets/Scripts/FloorRotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorRotate : MonoBehaviour
+{
+    [SerializeField]
+    private float rotateSpeed;
+
+    private void Update()
+    {
+        float rotate_x = transform.rotation.eulerAngles.x;
+        if (Mathf.Abs(rotate_x - 90) >= 0.01 && rotate_x < 90)
+        {
+            rotate_x -= Time.deltaTime * rotateSpeed;
+            transform.rotation = Quaternion.Euler(rotate_x, 0, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(90, 0, 0);
+            GameObject.Find("Map").GetComponent<GameManager>().BuildFinish = true;
+            gameObject.GetComponent<FloorRotate>().enabled = false;
+        }
+    }
+}
